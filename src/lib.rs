9b4eee4e8f388b4d60c512b9484f2d07ce723e15
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2022-02-10 18:50:59 +0800

    init base module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
 //! # deta-rust
 //!
 //! (Unofficial) Deta SDK for Rust.
+
+use std::marker::PhantomData;
+
+#[cfg(feature = "base")]
+pub mod base;
+
+pub struct Deta<S: Service> {
+    project_id: String,
+    project_key: String,
+    phantom: PhantomData<S>,
+}
+
+impl<S: Service> Deta<S> {
+    pub fn new(project_id: String, project_key: String) -> Self {
+        Self { project_id, project_key, phantom: PhantomData }
+    }
+}
+
+pub trait Service {}
